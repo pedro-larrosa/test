66c957a6c4c531a74021a8feccc527e763102b88
Nitsuga31 <sanvi74agus@gmail.com>
2020-10-22 14:45:37 +0200

    un programa to wapo

--- hola.cs
+++ hola.cs
@@ -1,9 +1,13 @@
-using System;
-
-public class Hola
+public class Saludo
 {
-  public static void Main()
-  {
-    Console.WriteLine("Hola mundo!");
-  }
+ public static void Main()
+ {
+ System.Console.WriteLine("h");
+ int primerNumero;
+ int segundoNumero;
+ int suma;
+ primerNumero=234;
+ segundoNumero=567;
+ suma=primerNumero+segundoNumero;
+ }
 }
